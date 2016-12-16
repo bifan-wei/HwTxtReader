b898aadcc6855e2174955bd9625b2a8a648a1689
bifan-wei <1198330662@qq.com>
2016-12-16 23:16:19 +0800

    commit
    commit

--- HwTxtReader/src/hwtxtreader/bean/Txterror.java
+++
@@ -1,7 +0,0 @@
-package hwtxtreader.bean;
-
-public class Txterror {
-
-	public int txterrorcode;
-	public String message = "";
-}
--- HwTxtReader/src/hwtxtreader/main/PageSeparateListener.java
+++
@@ -1,9 +0,0 @@
-package hwtxtreader.main;
-
-public interface PageSeparateListener {
-	
-	public void onSeparateStart();
-
-	public void onSeparateDone();
-
-}
